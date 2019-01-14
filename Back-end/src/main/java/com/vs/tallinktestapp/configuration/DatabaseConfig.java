7fbf70fa22d5d854da92f0c8b96686c9ae13b36f
Vladimir Semjonov <vbednoi@gmail.com>
2019-01-14 15:38:37 +0200

    attempts to fix + clean up

---
+++ Back-end/src/main/java/com/vs/tallinktestapp/configuration/DatabaseConfig.java
@@ -0,0 +1,19 @@
+package com.vs.tallinktestapp.configuration;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.h2.server.web.WebServlet;
+
+@Configuration
+public class DatabaseConfig {
+	@Bean
+	ServletRegistrationBean h2servletRegistration()
+	{
+	    ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+	    registrationBean.addUrlMappings("/console/*");
+	    return registrationBean;
+	}
+	 
+}
---
+++ Back-end/src/main/java/com/vs/tallinktestapp/controller/ParticipantController.java
@@ -0,0 +1,53 @@
+package com.vs.tallinktestapp.controller;
+
+import com.vs.tallinktestapp.transfer.*;
+import com.vs.tallinktestapp.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/api/public/participant")
+public class ParticipantController {
+    
+	@Autowired
+    private ParticipantService participantService;
+
+    @Autowired
+    private ConferenceService conferenceService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<Page<ParticipantDataTransferObject>> getAllPParticipant(Pageable pageable){
+        Page<ParticipantDataTransferObject> page = this.participantService.findParticipant(pageable);
+        return new ResponseEntity<>(page, HttpStatus.OK);
+    }
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<ParticipantDataTransferObject> getParticipant(@PathVariable Long id) {
+        return this.participantService.getParticipant(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteParticipant(@PathVariable Long id) {
+        this.participantService.deleteParticipant(id);
+    }
+
+    @RequestMapping(value = "/conference/{conferenceId}", method = RequestMethod.POST)
+    public void createParticipant(@RequestBody ParticipantDataTransferObject participantDTO, @PathVariable Long conferenceId) {
+        Optional<ConferenceDataTransferObject> conferenceDTO = this.conferenceService.getConference(conferenceId);
+        if (conferenceDTO.isPresent()){
+            conferenceDTO.get().getParticipants().add(participantDTO);
+            this.conferenceService.saveConference(conferenceDTO.get());
+        }
+    }
+   
+}
